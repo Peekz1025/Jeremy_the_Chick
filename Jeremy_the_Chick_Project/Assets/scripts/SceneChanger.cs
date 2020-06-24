57ed3b07e525621faa9f4d1bcfaa35428ee0b03c
Rob Piwko <peekz1025@yahoo.com>
2020-06-24 15:15:10 -0400

    implimented simple scene change

---
+++ Jeremy_the_Chick_Project/Assets/scripts/SceneChanger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+
+    public void changscene() {
+
+        SceneManager.LoadScene("game");
+    }
+}
