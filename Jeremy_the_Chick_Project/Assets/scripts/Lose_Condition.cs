24765f63d5c95eb39da9b428c0dbc674965d1f54
Rob Piwko <peekz1025@yahoo.com>
2020-07-14 23:50:05 -0400

    created my own branch. lose condition in place, needs a way to stop background activity when canvas comes up.  also need to reset game state when closing canvas

---
+++ Jeremy_the_Chick_Project/Assets/scripts/Lose_Condition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lose_Condition : MonoBehaviour
+{
+    public GameObject jeremyPrefab;
+    public Canvas lose_canvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (jeremyPrefab.transform.position.y < -10)
+        {
+            lose_canvas.enabled = true;
+            Debug.Log("you lost");
+        }
+    }
+}
