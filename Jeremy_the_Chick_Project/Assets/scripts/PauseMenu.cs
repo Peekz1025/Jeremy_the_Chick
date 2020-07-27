996050e09f328cc28b11f6153b768dd752fe1b87
Rob Piwko <peekz1025@yahoo.com>
2020-07-26 20:28:59 -0400

    pause menu working

---
+++ Jeremy_the_Chick_Project/Assets/scripts/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+    
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
