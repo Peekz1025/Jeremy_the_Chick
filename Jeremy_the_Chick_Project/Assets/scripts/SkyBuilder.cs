63915240fe103546798d19744ed9a24e8ea7e8c0
Rob Piwko <peekz1025@yahoo.com>
2020-11-09 21:17:36 -0500

    created backgrounds like terrain spawning.  still need to make them move with jeremy Y pos

---
+++ Jeremy_the_Chick_Project/Assets/scripts/SkyBuilder.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBuilder : MonoBehaviour
+{
+    public int levelSize;
+    public Vector3 buildPoint;
+    private GameObject[] pieces;
+
+    //track jeremy postiton
+    GameObject Jeremy;
+    Vector3 jeremyPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Jeremy = GameObject.FindGameObjectWithTag("TheJeremy");
+        jeremyPosition = Jeremy.transform.position;
+
+        pieces = Resources.LoadAll<GameObject>("sky_pieces/");
+        buildWorld(levelSize, buildPoint);
+    }
+
+    void Update()
+    {
+        //track jeremy's current position
+        jeremyPosition = Jeremy.transform.position;
+
+        //if jeremy is close and its not the first spawn
+        if (jeremyPosition.x >= buildPoint.x - 20 && buildPoint.x != -2)
+        {
+            buildWorld(levelSize, buildPoint);
+        }
+    }
+
+    private void buildWorld(int size, Vector2 start)
+    {
+        WorldPiece prev = null;
+        for(int i = 0; i != size; i++)
+        {           
+            GameObject go = getRandomPiece();
+            WorldPiece piece = GameObject.Instantiate(go).GetComponent<WorldPiece>();
+            if(i != 0) {
+                piece.prev = prev;
+                prev.next = piece;
+                piece.attachToAnchor(prev.rightAnchor.position);
+                buildPoint = piece.rightAnchor.position;
+            }
+            else
+            {
+                piece.attachToAnchor(start);
+            }
+
+            prev = piece;
+        }
+    }
+
+    //shouldnt need
+    private void firstBuild(int size, Vector2 start)
+    {
+        GameObject first = pieces[0];
+        WorldPiece firstPiece = GameObject.Instantiate(first).GetComponent<WorldPiece>();
+        firstPiece.attachToAnchor(start);
+
+        WorldPiece prev = firstPiece;
+        for (int i = 0; i != size; i++)
+        {
+            GameObject go = getRandomPiece();
+            WorldPiece piece = GameObject.Instantiate(go).GetComponent<WorldPiece>();
+            piece.prev = prev;
+            prev.next = piece;
+            piece.attachToAnchor(prev.rightAnchor.position);
+            buildPoint = piece.rightAnchor.position;
+            prev = piece;
+        }
+    }
+
+    private GameObject getRandomPiece()
+    {
+        int id = Random.Range(0, pieces.Length);
+
+        return pieces[id];
+    }
+
+}
